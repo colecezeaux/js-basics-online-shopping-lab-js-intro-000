f6f7ebe94fc895c03e0b6c8dfffc8fe1bb4fe6d9
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-05 19:37:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,10 +61,10 @@ function removeFromCart(item) {
   for(i = 0; i < cart.length; i++) {
   }
   if(cart[i].hasOwnProperty(item)) {
-      output = cart
-      cart.splice([i], 1);
+      output = cart.splice([i], 1);
+      return cart
     }
-  return output
+    return output
 }
 
 function placeOrder(cardNumber) {
