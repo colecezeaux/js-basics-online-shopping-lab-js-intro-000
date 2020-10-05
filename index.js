4aa1b2f4c913263b1ac418183c24342686848e6c
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-05 20:07:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  var output;
   if(cardNumber == undefined) {
     output = "Sorry, we don't have a credit card on file for you."
   } else {
