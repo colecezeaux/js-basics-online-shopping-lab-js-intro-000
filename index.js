e5ac2892f2b66d13e73dbfcef3aec5798d36ad77
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-05 20:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function placeOrder(cardNumber) {
   if(cardNumber == undefined) {
     output = "Sorry, we don't have a credit card on file for you."
   } else {
-    output = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    output = `Your total cost is $ ${total()}, which will be charged to the card ${cardNumber}.`
   }
   return output
 }
