0db34d0c621cf252df72b07ff17f671b15b62efd
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-05 20:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ function placeOrder(cardNumber) {
   } else {
     output = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
-  delete.cart
+  delete cart.itemName
   return output
 }
